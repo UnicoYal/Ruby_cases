7f9ead1dcd3cb11bf269ad3d1b63f62b5ef87096
CodeIgor31 <yagmurov.igor@mail.ru>
2023-02-27 18:41:55 +0300

    Rdy

---
+++ Set/main.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Case
+module Sets
+  def self.gen_diff(data1, data2)
+    keys = data1.keys | data2.keys
+
+    keys.each_with_object({}) do |key, acc|
+      acc[key] = if !data1.key?(key)
+                  'added'
+                elsif !data2.key?(key)
+                  'deleted'
+                elsif data1[key] == data2[key]
+                  'unchanged'
+                else
+                  'changed'
+                end
+    end
+  end
+end
---
+++ Set/test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'main'
+
+RSpec.describe Sets do
+  describe 'function' do
+    it 'should works' do
+      actual = described_class.gen_diff(
+        { one: 'eon', two: 'two', four: true },
+        { two: 'own', zero: 4, four: true }
+      )
+  
+      expected = {
+        one: 'deleted',
+        two: 'changed',
+        four: 'unchanged',
+        zero: 'added'
+      }
+  
+      expect { actual == expected }
+    end
+  end
+end
